d8350b383c659c3ac48cb9e1008b570ff76f2c5a
AgnesMuita <wamwithamuita@gmail.com>
2020-08-12 19:45:18 +0300

    add changes to load entries from local storage instead of creating an empty array

--- JS/script.js
+++ JS/script.js
@@ -5,28 +5,54 @@ var addButton = document.getElementById ("#add");
 var saveEntry = document.getElementById ("#isOutput")
 this.entries = [];
 
-// function Diary (date, name, description){
-//     this.dateInput = document.getElementById ('#date');
-//     this.nameInput= document.getElementById ('#name');
-//     this.descriptionInput = document.getElementById ('#summernote');
-//     this.addButton = document.getElementById ('#add');
-//     this.saveEntry = document.getElementById ('#isOutput');
-//     this.setEvents();
-// }
+function Diary (date, name, description){
+    const storageEntries = localStorage.getItem("entries"); //loads entries from local storage instead of creating an empty array.
+    this.entries = storageEntries  ? JSON.parse (storageEntries):[]; //store our entries from local storage to storageEntries variable.
+    this.dateInput = date;
+    this.nameInput= name;
+    this.descriptionInput = description;
+    this.addButton = addButton;
+    this.saveEntry = document.getElementById ('#isOutput');
+    this.setEvents();
+}
 function setEvents(){
     this.addButton.onclick = (event) => {
             event.preventDefault();
             var entry = newEntry(date.value, name.value, description.value);
-            this.entries.push(entry);
-            this.saveEntry();
-            console.log(entry)
-    };
+            this.entries.push(entry);  
+            localStorage.setItem ("entries", JSON.stringify(this.entries)); //store new entries in entries array and local storage since working only with the latter would require constant conversion of object entries to strings and back. 
+            this.saveEntry();   
+         };
 };
 
 function saveEntry(){
    this.saveEntry.innerHTML = "";
    for (let i= 0; i< this.entries.length; i++){
        var entry = this.entries[i];
-       this.saveEntry.innerHTML += `<h2>${entry.name}</h2>when: ${entry.date}<br>do:${entry.description}`;
+       this.saveEntry.innerHTML += `<h2>${entry.name}</h2>when:${entry.date}<br>do:${entry.description}`;
    };
 };
+// function getEntries(){
+//     var entries = [];
+//     for (var i = 0; i < entries.length; i++){
+//         $('#isOutput').append(entries[i]);
+//     }
+// }
+// $ ('#archive').click(function(){
+//     getEntries1();
+// });
+
+// $('#add').on('click', function(){
+//     $('input').each (function(){
+//         var id = $(this).attr ('id');
+//         var value = $(this).val();
+//         localStorage.setItem(id,value);
+//     });
+// });
+// $('#load').on('click', function(){
+//     $('input').each(function(){
+//         var id = $(this).attr('id');
+//         var value = localStorage.getItem(id);
+//         $(this).val(value);
+//     });
+// });
